42154f100f2234aaf6291818a078a714845eb3a7
¨samuel <nshernandezh@correo.url.edu.gt>
2025-05-23 08:00:35 -0600

    Segundo Problema

--- src/main/java/ed/lab/E01MeetingRooms.java
+++ src/main/java/ed/lab/E01MeetingRooms.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class E01MeetingRooms {
 
     public int minMeetingRooms(List<MeetingInterval> meetingIntervals) {
+
         return -1;
     }
 
--- src/main/java/ed/lab/E02Logger.java
+++ src/main/java/ed/lab/E02Logger.java
@@ -1,12 +1,32 @@
 package ed.lab;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class E02Logger {
+    private final Map<String, Integer> messageTimestamps;
 
     public E02Logger() {
+        messageTimestamps = new HashMap<>();
+
+
     }
 
     public boolean shouldPrintMessage(int timestamp, String message) {
+
+        if (!messageTimestamps.containsKey(message)) {
+            messageTimestamps.put(message, timestamp);
+            return true;
+        }
+
+        int lastPrintedTime = messageTimestamps.get(message);
+        if (timestamp >= lastPrintedTime + 10) {
+            messageTimestamps.put(message, timestamp);
+            return true;
+        }
+
+
         return false;
     }
-
 }
+
